6bdad4b3d14cd64d15499c41f4cb99de4fafa111
Ana Beatriz Santucci Carmoni <anabeatrizscarmoni@gmail.com>
2024-10-13 12:26:19 -0300

    feat: configure CORS

--- src/main/java/com/example/api/config/WebConfig.java
+++ src/main/java/com/example/api/config/WebConfig.java
@@ -1,23 +1,40 @@
 package com.example.api.config;
 
 import com.example.api.serialization.converter.YamlJackson2HttpMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer{
+
     private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
+
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
 
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new YamlJackson2HttpMessageConverter());
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+                //.allowedMethods("GET", "POST", "PUT")
+                .allowedMethods("*")
+                .allowedOrigins(allowedOrigins)
+                .allowCredentials(true);
+    }
+
+    @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         // https://www.baeldung.com/spring-mvc-content-negotiation-json-xml
         // Via EXTENSION. http://localhost:8080/api/person/v1.xml DEPRECATED on SpringBoot 2.6
@@ -43,4 +60,5 @@ public class WebConfig implements WebMvcConfigurer {
                 .mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML)
         ;
     }
+
 }

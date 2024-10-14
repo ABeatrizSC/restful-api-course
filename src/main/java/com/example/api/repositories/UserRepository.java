ad28673eba20bd58e4daceeae1e0777306f11c89
Ana Beatriz Santucci Carmoni <anabeatrizscarmoni@gmail.com>
2024-10-14 11:13:23 -0300

    feat: implement user service and repository

---
+++ src/main/java/com/example/api/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.api.repositories;
+
+import com.example.api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u from User WHERE u.userName =:userName")
+    User findByUsername(@Param("userName") String userName);
+}
